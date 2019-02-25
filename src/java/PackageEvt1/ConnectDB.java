faa8842ef455ad81fb2004bfa1d31576d3e9543f
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-02-25 17:41:26 -0600

    rec

--- src/java/PackageEvt1/ConnectDB.java
+++ src/java/PackageEvt1/ConnectDB.java
@@ -21,7 +21,7 @@ public class ConnectDB
     public Connection getConnection(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/plataformaweb?useSSL=false&serverTimezone=CST", "root", "12345");
+            Connection conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "12345");
             return conn;
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
