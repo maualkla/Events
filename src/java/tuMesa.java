6ea7aca87119b0da940d3a6addeab20dfb77ac0a
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-02-28 15:45:21 -0600

    siguiente

--- src/java/tuMesa.java
+++ src/java/tuMesa.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 
+import PackageEvt1.DBActions;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,13 +36,30 @@ public class tuMesa extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+           
+            DBActions db = new DBActions();
+            Connection conn = db.getConnection();
+            String invitado = request.getParameter("invitado_id");
+            if(conn==null)
+            {
+                     out.print("Connection failed");
+            }
+            ResultSet a_invitado = db.foundInvitado(conn, invitado);
+            ResultSet a_mesa = db.foudTable(conn, invitado);
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet tuMesa</title>" + request.getParameter("invitado_id")); 
+            out.println("<title> Events Tu Mesa </title>" + request.getParameter("invitado_id")); 
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet tuMesa at " + request.getContextPath() + "</h1>");
+            out.println("<style>"
+                    + " body {background-color: #ECEFF1; font-family: sans-serif; text-align: center; font-size: 10px; color: #245A64;}"
+                    + ".conexion_status{padding: 20px; background-color: #000000;border-radius: 5px;width: 20%;color:#ffffff;margin-left: 38.5%;margin-right: auto;} "
+                    + ".boton{background: #AFC5FF;border-radius: 3px;border: none;color: white;padding: 5px;cursor: pointer;margin:5px;}"
+                    + "</style>"
+                    + " Bienvenido " + invitado
+                    + "invitado" + a_invitado 
+                    + "mesa" + a_mesa);
             out.println("</body>");
             out.println("</html>");
         }
